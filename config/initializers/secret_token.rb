cb72004c426a3b06fec6af582568b965849301ec
SMH Abdoli <smh.abdoli@gmail.com>
2013-10-04 23:32:04 +0330

    initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Greenbook::Application.config.secret_key_base = '7089b386b37dd30eec8dc7086c0ec2fa6f96f13731c5b7d04c38539428d55f2fec35d18c64c581d691e3dc73e82ec30efb368e0cb29ae28cbe9fc9fdc8377fcf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Greenbook::Application.config.session_store :cookie_store, key: '_greenbook_session'
